a6668e390b5e8d9b5fdc3c12d1f41a70c55789c2
TheDeveloper10 <th3r3ap3r@abv.bg>
2022-09-05 17:21:34 +0300

    Added missing file from previous commit and updated todos

--- internal/util/test/layerTest.go
+++ internal/util/test/layerTest.go
@@ -1,1 +1,45 @@
 package test
+
+import (
+	"encoding/json"
+	"github.com/TheDeveloper10/rem"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"notification-service/internal/helper"
+	"notification-service/internal/util/iface"
+	"strings"
+	"testing"
+)
+
+type LayerTestCase struct {
+	ExpectedStatus int
+	SetHeader      bool
+	Body 		   iface.IRequest
+}
+
+func (ltc *LayerTestCase) PrepareTest(t *testing.T) (rem.IRequest, rem.IResponse) {
+	var body io.Reader = nil
+
+	if ltc.Body != nil {
+		bodyBytes, _ := json.Marshal(ltc.Body)
+		body = strings.NewReader(string(bodyBytes))
+	}
+
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest("POST", "", body)
+	if helper.IsError(err) {
+		t.Fatal(err.Error())
+	}
+
+	if ltc.SetHeader {
+		req.Header.Add("Content-Type", "application/json")
+	}
+
+	response := rem.NewHTTPResponseWriter(rec)
+	request := rem.NewBasicRequest(req)
+
+	response.Status(http.StatusOK)
+
+	return request, response
+}
