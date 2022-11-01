4c9353f1c174c2660c49e85e8f8a68ae133882e4
TheDeveloper10 <th3r3ap3r@abv.bg>
2022-11-01 21:16:00 +0200

    Removed scripts/ and replaced it with a better one called example/

--- scripts/build/build-and-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Building..."
-go build .
-
-echo "Running..."
-./notification-service
--- scripts/docker/rebuild-backend-only.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd ../../
-go build .
-cd scripts/docker
-sudo docker compose restart backend
