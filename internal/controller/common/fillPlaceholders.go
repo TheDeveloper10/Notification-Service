c0971c6e3917659b68090a241e4bd136000176af
TheDeveloper10 <th3r3ap3r@abv.bg>
2022-11-07 18:23:49 +0200

    Added a common package to the controllers folder and fixed some issues

---
+++ internal/controller/common/fillPlaceholders.go
@@ -0,0 +1,50 @@
+package processor
+
+import (
+	"notification-service/internal/data/dto"
+	"notification-service/internal/data/entity"
+	"notification-service/internal/util"
+	"strings"
+)
+
+func FillPlaceholders(text string, placeholders *[]dto.TemplatePlaceholder) (*string, error) {
+	for i := 0; i < len(*placeholders); i++ {
+		placeholder := &((*placeholders)[i])
+		if err := placeholder.Validate(); err != nil {
+			return nil, err
+		}
+
+		key := "@{" + placeholder.Key + "}"
+		text = strings.ReplaceAll(text, key, placeholder.Value)
+	}
+
+	return &text, nil
+}
+
+func FillPlaceholdersOnTemplate(template *entity.TemplateEntity, placeholders *[]dto.TemplatePlaceholder) error {
+	if template.Body.Email != nil {
+		edited, err := FillPlaceholders(*template.Body.Email, placeholders)
+		if util.ManageError(err) {
+			return err
+		}
+		template.Body.Email = edited
+	}
+
+	if template.Body.SMS != nil {
+		edited, err := FillPlaceholders(*template.Body.SMS, placeholders)
+		if util.ManageError(err) {
+			return err
+		}
+		template.Body.SMS = edited
+	}
+
+	if template.Body.Push != nil {
+		edited, err := FillPlaceholders(*template.Body.Push, placeholders)
+		if util.ManageError(err) {
+			return err
+		}
+		template.Body.Push = edited
+	}
+
+	return nil
+}
---
+++ internal/controller/common/getPlaceholders.go
@@ -0,0 +1,42 @@
+package processor
+
+import (
+	"github.com/sirupsen/logrus"
+	"regexp"
+)
+
+func GetPlaceholders(text *string) string {
+	regex, err := regexp.Compile("@{[^$]*?}")
+	if err != nil {
+		logrus.Error("Failed to compile regex")
+		return ""
+	}
+
+	matches := regex.FindAllString(*text, -1)
+	if len(matches) <= 0 {
+		return ""
+	}
+
+	type void struct{}
+	var empty void
+
+	first := true
+	res := ""
+	set := make(map[string]void)
+	for _, placeholder := range matches {
+		placeholder = placeholder[2:len(placeholder)-1]
+
+		if _, ok := set[placeholder]; !ok {
+			if first {
+				res += placeholder
+				first = false
+			} else {
+				res += ", " + placeholder
+			}
+		} else {
+			set[placeholder] = empty
+		}
+	}
+
+	return res
+}
